8dc3a117198b7e310f60363664de4fde8810f425
TonyGomez23 <72.tonygomez@gmail.com>
2019-10-28 18:03:03 -0400

    Hi

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
+// $(".output").text("");
+    let nums=$("input").val();
+    let nums_array = nums.split('');
+    console.log(nums_array);
 
+        let sum = 0;
+        nums_array.forEach(function(element){
+            sum += element;
+        });
 
+    $('.output').append(nums + "=" + sum);
+    let 
 
-  
-  
-  
-  
 });
+});
+
